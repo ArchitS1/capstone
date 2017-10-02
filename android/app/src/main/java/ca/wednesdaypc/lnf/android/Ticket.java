f155434f8237cb56ccff2860cd3ebd07be68c6b7
Anshita Sharma <anshitasharma40@gmail.com>
2017-10-01 21:43:10 -0400

    Add and update following things in Ticket Activity:- -Textview -Spinner -Add categories in spinner -Update formatting

--- android/app/src/main/java/ca/wednesdaypc/lnf/android/Ticket.java
+++ android/app/src/main/java/ca/wednesdaypc/lnf/android/Ticket.java
@@ -4,6 +4,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+
 
 public class Ticket extends AppCompatActivity {
 
@@ -11,6 +19,34 @@ public class Ticket extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ticket);
+
+
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        List<String> list= new ArrayList<String>();
+        list.add("Animals / Pets");
+        list.add("Bags, Baggage, Luggage");
+        list.add("Clothing");
+        list.add("Collectors Items");
+        list.add("Currency / Money");
+        list.add("Documents / Literature");
+        list.add("Electronics");
+        list.add("Household / Tools");
+        list.add("Jewelry");
+        list.add("Mail / Parcel");
+        list.add("Media");
+        list.add("Medical");
+        list.add("Musical Equipment");
+        list.add("Personal Accessories");
+        list.add("Sporting Goods");
+        list.add("Toys");
+
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, list);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        spinner.setAdapter(adapter);
+
 
     }
 }
